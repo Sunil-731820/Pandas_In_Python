c2baca68f2baf1c0b398285fa2691ae84851e18d
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2022-05-13 22:43:39 +0530

    coded

---
+++ BasicMatplotlib.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [1,2,3,4,5,6]
+y = [10,11,12,13,14,16]
+plt.plot(x,y)
+plt.title("Simple Line Graph")
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+plt.show()
---
+++ LineGraph1.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5,6],[11,13,12,14,15,16])
+plt.axis([0,10,0,20])
+plt.show()
